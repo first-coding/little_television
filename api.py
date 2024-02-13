54b7f75374202e60d4a32c5c2937c4aadaa74b48
wangdalin <731994586@qq.com>
2024-02-13 20:50:30 +0800

    修改查数和更新数据

--- api.py
+++ api.py
@@ -15,9 +15,9 @@ class mysql_handler:
         self.mysql_port = mysql_port
         self.mysql_db = mysql_db
         self.url = f'mysql+pymysql://{self.mysql_name}:{self.mysql_password}@{self.mysql_host}:{self.mysql_port}/{self.mysql_db}'
-    def search_data(self, table_name):
+    def search_data(self, table_name, user_id):
         engine = create_engine(self.url)
-        query_search = f"select * from {table_name}"
+        query_search = f"select * from {table_name} where user_id = {user_id}"
         search_df = pd.read_sql_query(query_search, engine)
         return search_df
     
@@ -93,18 +93,25 @@ class api:
 
         @self.app.post("/get_data")
         async def get_data(request: Request):
-            search_df = self.mysql_instance.search_data('files_data')
+            json_raw = await request.json()
+            json_middle = json.loads(json.dumps(json_raw))
+            user_id = json_middle.get("user_id")
+            table_name = json_middle.get("table")
+            search_df = self.mysql_instance.search_data(table_name, user_id)
             return json.loads(search_df.to_json(orient='records',force_ascii=False))
 
         @self.app.post("/update_data")
         async def update_data(request: Request):
             json_raw = await request.json()
-            data = json.loads(json.dumps(json_raw)).get("data")
-            judgement = self.mysql_instance.update_data('files_data',data)
+            json_middle = json.loads(json.dumps(json_raw))
+            data = json_middle.get("data")
+            table_name = json_middle.get("table")
+            judgement = self.mysql_instance.update_data(table_name, data)
             if judgement:
                 return {"status": "success"}
             else:
                 return {"status": "fail"}
+        
 
     
 if __name__ == '__main__':
